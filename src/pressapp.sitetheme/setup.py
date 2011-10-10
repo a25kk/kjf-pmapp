0ca1e5cc3b590f8e82c9e2bf409c3978599594cf
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-10-10 16:20:30 +0200

    Default theme setup (based on twitter bootstrap blueprint, still missing files)

--- src/pressapp.sitetheme/setup.py
+++ src/pressapp.sitetheme/setup.py
@@ -25,6 +25,8 @@ setup(name='pressapp.sitetheme',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.app.theming',
+          'plone.app.themingplugins',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
