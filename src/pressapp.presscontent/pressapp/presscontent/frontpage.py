5ed5aef6be844ebec88907141b92f983acfd1b1a
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-10-19 19:25:17 +0200

    Add a grok.View that can be used on `INavigationRoot` to redirect to the login form or the press center respectively.

---
+++ src/pressapp.presscontent/pressapp/presscontent/frontpage.py
@@ -0,0 +1,24 @@
+from five import grok
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from plone.app.layout.navigation.interfaces import INavigationRoot
+
+class FrontpageView(grok.View):
+    grok.context(INavigationRoot)
+    grok.require('zope2.View')
+    grok.name('frontpage-view')
+    
+    def render(self):
+        context = aq_inner(self.context)
+        mtool = getToolByName(context, 'portal_membership')
+        if mtool.isAnonymousUser():
+            return self.request.response.redirect(context.absolute_url() + "/login_form")
+        else:
+            try:
+                home_url = mtool.getAuthenticatedMember().getHomeFolder().absolute_url()
+                return self.request.response.redirect(home_url)
+            except:
+                home_url = mtool.getMembersFolder().absolute_url()
+                return self.request.response.redirect(home_url)
+    
+        
