40e83f6348f784bff0acd2cb74a88a6c019bbbd4
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-01-16 15:37:54 +0100

    Cleanup the paster generated content type layout/structure

--- src/pressapp.channelmanagement/pressapp/channelmanagement/channel.py
+++ src/pressapp.channelmanagement/pressapp/channelmanagement/channel.py
@@ -27,39 +27,13 @@ class IChannel(form.Schema, IImageScaleTraversable):
     """
     A specific channel holding subscriber objects
     """
-    
-    # If you want a schema-defined interface, delete the form.model
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/channel.xml to define the content type
-    # and add directives here as necessary.
-    
-    form.model("models/channel.xml")
 
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Channel(dexterity.Container):
     grok.implements(IChannel)
-    
-    # Add your class methods and properties here
 
 
-# View class
-# The view will automatically use a similarly named template in
-# channel_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(IChannel)
     grok.require('zope2.View')
-    
-    # grok.name('view')
+    grok.name('view')
--- src/pressapp.channelmanagement/pressapp/channelmanagement/channelmanager.py
+++ src/pressapp.channelmanagement/pressapp/channelmanagement/channelmanager.py
@@ -21,45 +21,19 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from pressapp.channelmanagement import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IChannelManager(form.Schema, IImageScaleTraversable):
     """
     Central managing unit for subscriber channels
     """
-    
-    # If you want a schema-defined interface, delete the form.model
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/channelmanager.xml to define the content type
-    # and add directives here as necessary.
-    
-    form.model("models/channelmanager.xml")
 
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class ChannelManager(dexterity.Container):
     grok.implements(IChannelManager)
-    
+
     # Add your class methods and properties here
 
 
-# View class
-# The view will automatically use a similarly named template in
-# channelmanager_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(IChannelManager)
     grok.require('zope2.View')
-    
-    # grok.name('view')
+    grok.name('view')
