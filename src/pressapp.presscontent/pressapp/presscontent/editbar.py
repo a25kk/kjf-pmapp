84ee72d2a3015146410194ca8d681193a0e79c3d
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-01-18 16:42:10 +0100

    Additional edit bar for the press center view

--- src/pressapp.presscontent/pressapp/presscontent/editbar.py
+++ src/pressapp.presscontent/pressapp/presscontent/editbar.py
@@ -1,15 +1,35 @@
 from five import grok
 from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets.interfaces import IAboveContent
+from pressapp.presscontent.presscenter import IPressCenter
 from pressapp.presscontent.pressroom import IPressRoom
+
 
 class EditBarViewlet(grok.Viewlet):
     grok.name('pressapp.membercontent.EditBarViewlet')
     grok.context(IPressRoom)
     grok.require('zope2.View')
     grok.viewletmanager(IAboveContent)
-    
+
     def update(self):
         context = aq_inner(self.context)
         self.context_url = context.absolute_url()
-    
+
+
+class PressCenterEditBarViewlet(grok.Viewlet):
+    grok.name('pressapp.membercontent.PressCenterEditBarViewlet')
+    grok.context(IPressCenter)
+    grok.require('zope2.View')
+    grok.viewletmanager(IAboveContent)
+
+    def update(self):
+        context = aq_inner(self.context)
+        self.context_url = context.absolute_url()
+
+    def my_workspace(self):
+        context = aq_inner(self.context)
+        mtool = getToolByName(context, 'portal_membership')
+        member = mtool.getAuthenticatedMember()
+        home_url = member.getHomeFolder().absolute_url()
+        return home_url
