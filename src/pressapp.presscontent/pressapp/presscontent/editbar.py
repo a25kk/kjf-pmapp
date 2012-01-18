c7cfd73c8f7293cfb241ca31c3a70277864f9087
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-01-18 19:11:45 +0100

    New edit bar viewlet for the channel manager

--- src/pressapp.presscontent/pressapp/presscontent/editbar.py
+++ src/pressapp.presscontent/pressapp/presscontent/editbar.py
@@ -1,9 +1,11 @@
 from five import grok
 from Acquisition import aq_inner
+from Acquisition import aq_parent
 from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets.interfaces import IAboveContent
 from pressapp.presscontent.presscenter import IPressCenter
 from pressapp.presscontent.pressroom import IPressRoom
+from pressapp.channelmanagement.channelmanager import IChannelManager
 
 
 class EditBarViewlet(grok.Viewlet):
@@ -26,6 +28,25 @@ class PressCenterEditBarViewlet(grok.Viewlet):
     def update(self):
         context = aq_inner(self.context)
         self.context_url = context.absolute_url()
+
+    def my_workspace(self):
+        context = aq_inner(self.context)
+        mtool = getToolByName(context, 'portal_membership')
+        member = mtool.getAuthenticatedMember()
+        home_url = member.getHomeFolder().absolute_url()
+        return home_url
+
+
+class ChannelEditBarViewlet(grok.Viewlet):
+    grok.name('pressapp.membercontent.ChannelEditBarViewlet')
+    grok.context(IChannelManager)
+    grok.require('zope2.View')
+    grok.viewletmanager(IAboveContent)
+
+    def update(self):
+        context = aq_inner(self.context)
+        self.context_url = context.absolute_url()
+        self.parent_url = aq_parent(context).absolute_url()
 
     def my_workspace(self):
         context = aq_inner(self.context)
