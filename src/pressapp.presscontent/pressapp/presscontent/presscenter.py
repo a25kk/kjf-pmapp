96a0206be2b56d419bdfebbf21b27a876bb29010
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-01-18 16:43:08 +0100

    Press center view refactoring and inclusion of new channel manager subtype

--- src/pressapp.presscontent/pressapp/presscontent/presscenter.py
+++ src/pressapp.presscontent/pressapp/presscontent/presscenter.py
@@ -7,6 +7,10 @@ from plone.memoize.instance import memoize
 
 from z3c.form import group, field
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+from Products.CMFCore.utils import getToolByName
+
+from pressapp.presscontent.pressroom import IPressRoom
+
 from pressapp.presscontent import MessageFactory as _
 
 
@@ -20,16 +24,28 @@ class View(grok.View):
     grok.context(IPressCenter)
     grok.require('zope2.View')
     grok.name('view')
-    
+
     def update(self):
         self.has_pressrooms = len(self.contained_pressrooms()) > 0
         self.pressrooms = self.contained_pressrooms()
-    
+
     def contained_pressrooms(self):
         context = aq_inner(self.context)
-        items = context.items()
-        return items
-    
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(object_provides=IPressRoom.__identifier__,
+                          path=dict(query='/'.join(context.getPhysicalPath()),
+                                    depth=1),
+                          sort_on='sortable_title')
+        return results
+
+    def memberinfo(self, owner):
+        context = aq_inner(self.context)
+        mtool = getToolByName(context, 'portal_membership')
+        member = mtool.getMemberById(owner)
+        login_time = member.getProperty('last_login_time', '2011/01/01')
+        return login_time
+
     @memoize
     def can_edit(self):
-        return bool(getSecurityManager().checkPermission('Portlets: Manage own portlets', self.context))
+        return bool(getSecurityManager().checkPermission(
+                    'Portlets: Manage own portlets', self.context))
