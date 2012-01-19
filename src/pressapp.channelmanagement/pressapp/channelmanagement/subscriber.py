63adad4b1ab96ac83417efde7238d855d07e5534
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-01-19 18:59:53 +0100

    Re-factor the channel source binder into a standalone vocabulary that is re-usable across content types

--- src/pressapp.channelmanagement/pressapp/channelmanagement/subscriber.py
+++ src/pressapp.channelmanagement/pressapp/channelmanagement/subscriber.py
@@ -2,63 +2,14 @@ from five import grok
 from plone.directives import dexterity, form
 
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary
-from zope.component import queryUtility
 from zope.interface import invariant, Invalid
 
 from z3c.form import group, field
-from z3c.formwidget.query.interfaces import IQuerySource
 from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
-from plone.registry.interfaces import IRegistry
+
+from pressapp.channelmanagement.vocabulary import ChannelSourceBinder
 
 from pressapp.channelmanagement import MessageFactory as _
-
-
-class ChannelSource(object):
-    grok.implements(IQuerySource)
-
-    def __init__(self, context):
-        self.context = context
-        self.key = 'pressapp.channelmanagement.availableChannels'
-        self.channel_list = self.getChannelList()
-        self.vocab = SimpleVocabulary.fromItems(
-            [(x, x) for x in self.channel_list])
-
-    def __contains__(self, term):
-        return self.vocab.__contains__(term)
-
-    def __iter__(self):
-        return self.vocab.__iter__()
-
-    def __len__(self):
-        return self.vocab.__len__()
-
-    def getTerm(self, value):
-        return self.vocab.getTerm(value)
-
-    def getTermByToken(self, value):
-        return self.vocab.getTermByToken(value)
-
-    def search(self, query_string):
-        q = query_string.lower()
-        return [self.getTerm(kw)
-                for kw in self.channel_list if q in kw.lower()]
-
-    def getChannelList(self):
-        registry = queryUtility(IRegistry)
-        terms = []
-        if registry:
-            for value in registry.get(self.key, ()):
-                terms.append(value)
-        return terms
-
-
-class ChannelSourceBinder(object):
-    grok.implements(IContextSourceBinder)
-
-    def __call__(self, context):
-        return ChannelSource(context)
 
 
 class ISubscriber(form.Schema):
