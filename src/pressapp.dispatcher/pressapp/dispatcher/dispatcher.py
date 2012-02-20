25a28641fffed0ce5ae8669a6aec2f9365bf5876
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-02-20 17:15:26 +0100

    Add (empty) skim tempalte for the dispatcher for debugging reasons (we can print values) and include a send method inside the dispatcher module

--- src/pressapp.dispatcher/pressapp/dispatcher/dispatcher.py
+++ src/pressapp.dispatcher/pressapp/dispatcher/dispatcher.py
@@ -34,14 +34,90 @@ class Dispatcher(grok.View):
     grok.name('dispatcher')
 
     def update(self):
-        pass
+        self.recipients = self._getRecievers()
 
-    def render(self):
-        return ''
+    def send(self):
+        context = aq_inner(self.context)
+        mailhost = getToolByName(context, 'MailHost')
+        proptool = getToolByName(context, 'portal_properties')
+        charset = proptool.getProperty('default_charset')
+        send_counter = 0
+        send_error_counter = 0
+        recipients = self.recipients()
+        output_html = self._render_output_html()
+        rendered_email = self._exchange_relative_urls(output_html)
+        text = rendered_email['html']
+        plain_text = rendered_email['plain']
+        image_urls = rendered_email['images']
+        for recipient in recipients:
+            outer = MIMEMultipart('alternative')
+            outer['To'] = Header('<%s>' % safe_unicode(recipient['mail']))
+            recipient_name = self.safe_portal_encoding(recipient['name'])
+            personal_text = text.replace('[[SUBSCRIBER]]', str(recipient_name))
+            personal_text_plain = plain_text.replace('[[SUBSCRIBER]]',
+                                                     str(recipient_name))
+            outer['From'] = self.from_header()
+            outer['Subject'] = self.subject_header()
+            outer.epilogue = ''
+            text_part = MIMEMultipart('related')
+            text_part.attach(MIMEText(personal_text_plain, 'plain', charset))
+            html_part = MIMEMultipart('related')
+            html_text = MIMEText(personal_text, 'html', charset)
+            html_part.attach(html_text)
+            image_number = 0
+            reference_tool = getToolByName(context, 'reference_catalog')
+            for image_url in image_urls:
+                try:
+                    image_url = urlparse(image_url)[2]
+                    if 'resolveuid' in image_url:
+                        urlparts = image_url.split('/')[1:]
+                        uuid = urlparts.pop(0)
+                        o = reference_tool.lookupObject(uuid)
+                        if o and urlparts:
+                            o = o.restrictedTraverse(urlparts[0])
+                    else:
+                        o = self.restrictedTraverse(urllib.unquote(image_url))
+                except Exception, e:
+                    log.error('Could not resolve the image \"%s\": %s'
+                                % (image_url, e))
+                else:
+                    if hasattr(o, "_data"):
+                        image = MIMEImage(o._data)
+                    elif hasattr(o, "data"):
+                        image = MIMEImage(o.data)
+                    else:
+                        image = MIMEImage(o.GET())
+                    image["Content-ID"] = "<image_%s>" % image_number
+                    image_number += 1
+                    html_part.attach(image)
+            outer.attach(text_part)
+            outer.attach(html_part)
+            try:
+                mailhost.send(outer.as_string())
+                log.info("Sent newsletter to \"%s\"" % recipient['mail'])
+                send_counter += 1
+            except Exception, e:
+                log.info("Sending to \"%s\" failed" % recipient['mail'])
+                send_error_counter += 1
+        log.info("Dipatched to %s recipients with %s errors." % (send_counter,
+            send_error_counter))
+        if not hasattr(self.request, 'test') and not recipients:
+            wftool = getToolByName(context, 'portal_workflow')
+            if wftool.getInfoFor(context, 'review_state') == 'private':
+                wftool.doActionFor(context, 'publish')
 
     def _getRecievers(self):
         context = aq_inner(self.context)
-        return ''
+        recievers = getattr(context, 'recipients', '')
+        recipients = []
+        if recievers:
+            for address in recievers:
+                recipient = {}
+                recipient_email, recipient_name = address.split(',')
+                recipient['mail'] = recipient_email
+                recipient['name'] = recipient_name
+                recipients.append(recipient)
+        return recipients
 
     def _render_output_html(self):
         """ Return rendered newsletter
