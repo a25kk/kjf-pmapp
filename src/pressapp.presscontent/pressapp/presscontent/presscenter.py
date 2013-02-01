6f30240cdff72d7c1d3ede876209e117d9fb0195
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-02-01 01:29:19 +0100

    Port history helper view from jobtool

--- src/pressapp.presscontent/pressapp/presscontent/presscenter.py
+++ src/pressapp.presscontent/pressapp/presscontent/presscenter.py
@@ -1,4 +1,6 @@
+import json
 from five import grok
+from plone import api
 from plone.directives import form
 from Acquisition import aq_inner
 from zope import schema
@@ -6,8 +8,12 @@ from AccessControl import getSecurityManager
 from plone.memoize.instance import memoize
 
 from Products.CMFCore.utils import getToolByName
+from plone.app.layout.viewlets.content import ContentHistoryView
+
+from Products.CMFCore.interfaces import IContentish
 from plone.app.contentlisting.interfaces import IContentListing
 
+from pressapp.presscontent.interfaces import IPressContent
 from pressapp.channelmanagement.subscriber import ISubscriber
 from pressapp.presscontent.pressroom import IPressRoom
 
@@ -128,9 +134,9 @@ class Workspaces(grok.View):
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
         results = catalog(object_provides=IPressRoom.__identifier__,
-                         path=dict(query='/'.join(context.getPhysicalPath()),
-                                   depth=1),
-                         sort_on='sortable_title')
+                          path=dict(query='/'.join(context.getPhysicalPath()),
+                                    depth=1),
+                          sort_on='sortable_title')
         spaces = IContentListing(results)
         return spaces
 
@@ -144,5 +150,73 @@ class Workspaces(grok.View):
         info['home_page'] = member.getProperty('home_page', '')
         info['location'] = member.getProperty('location', '')
         info['last_login_time'] = member.getProperty('last_login_time',
-                                                         '2011/01/01')
+                                                     '2011/01/01')
         return info
+
+
+class PressCenterHistory(grok.View):
+    grok.context(IContentish)
+    grok.require('cmf.ModifyPortalContent')
+    grok.name('changes')
+
+    def render(self):
+        data = self.jobtool_history()
+        self.request.response.setHeader('Content-Type',
+                                        'application/json; charset=utf-8')
+        return json.dumps(data)
+
+    def pressapp_history(self):
+        history = []
+        status = self.status_list()
+        timestamps = list()
+        for x in status:
+            pit = x['timestamp']
+            timestamps.append(pit)
+        pit_sorted = sorted(timestamps, reverse=True)
+        for pit in pit_sorted:
+            for x in status:
+                if x['timestamp'] == pit:
+                    history.append(x)
+        return history
+
+    def status_list(self):
+        items = self.last_modified_content()
+        state_info = []
+        idx = 0
+        for item in items:
+            obj = item.getObject()
+            history = self.history_info(obj)
+            if len(history) > 0:
+                event = history[0]
+                idx += 1
+                info = {}
+                info['idx'] = idx
+                timestamp = event['time']
+                time = api.portal.get_localized_time(datetime=timestamp,
+                                                     long_format=False)
+                time_only = api.portal.get_localized_time(datetime=timestamp,
+                                                          time_only=True)
+                info['time'] = time
+                info['time_only'] = time_only
+                info['timestamp'] = timestamp.ISO8601()
+                actor = event['actor']
+                info['actor'] = actor['username']
+                info['action'] = event['transition_title']
+                info['title'] = item.Title
+                info['url'] = item.getURL()
+                #info['details'] = event
+                state_info.append(info)
+        return state_info
+
+    def history_info(self, item):
+        context = aq_inner(self.context)
+        chv = ContentHistoryView(item, context.REQUEST).fullHistory()
+        return chv
+
+    def last_modified_content(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        results = catalog(object_provides=IPressContent.__identifier__,
+                          sort_on='modified',
+                          sort_order='reverse',
+                          sort_limit=5)[:5]
+        return results
