3a253ed1f86c4e2e029c5e27e6156d7bde0b3cbe
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-02-24 19:59:51 +0100

    Update press content dispatch planing process

--- src/pressapp.sitetheme/pressapp/sitetheme/resources/js/pressapp.js
+++ src/pressapp.sitetheme/pressapp/sitetheme/resources/js/pressapp.js
@@ -197,6 +197,7 @@
                 /* Init the table */
                 oTable = $('#recipient-table').dataTable({
                     "bPaginate": false,
+                    "bRetrieve": true,
                     "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
                     "sPaginationType": "bootstrap",
                     "oLanguage": {
