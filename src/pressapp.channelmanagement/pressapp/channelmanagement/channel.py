6922692181bd01f177fa8e6a1d3f9b3a3f4bffff
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-04-04 00:49:07 +0200

    Cleanup and provide more custom views

--- src/pressapp.channelmanagement/pressapp/channelmanagement/channel.py
+++ src/pressapp.channelmanagement/pressapp/channelmanagement/channel.py
@@ -1,23 +1,8 @@
 from five import grok
 from Acquisition import aq_inner
-from plone.directives import dexterity, form
-
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
+from plone.directives import form
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 from Products.CMFCore.utils import getToolByName
 
 from plone.app.contentlisting.interfaces import IContentListing
@@ -25,8 +10,6 @@ from pressapp.channelmanagement.subscriber import ISubscriber
 
 from pressapp.channelmanagement import MessageFactory as _
 
-
-# Interface class; used to define content-type schema.
 
 class IChannel(form.Schema, IImageScaleTraversable):
     """
@@ -41,6 +24,7 @@ class View(grok.View):
 
     def update(self):
         self.has_subscribers = len(self.subscribers()) > 0
+        self.subscriber_index = len(self.subscribers())
 
     def subscribers(self):
         context = aq_inner(self.context)
