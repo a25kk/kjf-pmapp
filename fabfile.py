351cc5d3ee028a05c7e051a0aa54ade64a5f59f6
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-11 13:02:05 +0100

    Add conditions to job opening view

--- fabfile.py
+++ fabfile.py
@@ -52,7 +52,7 @@ def restart_haproxy():
 
 
 @task
-def ctl(*cmd):
+def supervisorctl(*cmd):
     """Runs an arbitrary supervisorctl command."""
     with cd(env.webserver):
         run('nice bin/supervisorctl ' + ' '.join(cmd))
@@ -69,12 +69,4 @@ def deploy():
     """ Deploy current master to production server """
     project.site.update()
     project.site.build()
-    project.cluster.restart_clients()
-
-
-@task
-def deploy_full():
-    """ Deploy current master to production and run buildout """
-    project.site.update()
-    project.site.build_full()
     project.cluster.restart_clients()
