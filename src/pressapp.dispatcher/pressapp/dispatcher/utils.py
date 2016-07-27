13a30e2368ce92b44874d9900d460fe0178de4a3
Christoph Böhner-Figas <boehner@team23.de>
2016-07-27 17:10:42 +0200

    Update postprocessing utility
    Cleanup all protocol hints in uri schemes by normalizing to the
    now defacto standard relative linking

--- src/pressapp.dispatcher/pressapp/dispatcher/utils.py
+++ src/pressapp.dispatcher/pressapp/dispatcher/utils.py
@@ -1,20 +1,28 @@
+# -*- coding: utf-8 -*-
+import re
 from lxml import etree
 from lxml import html
 
 
 def postprocess_emailtemplate(content):
+    """ Replace links in rendered email templates
+
+    Prevent 404 errors and unnecessary redirects by making the protocol
+    scheme relative in the first place
+
+    Note: this might put of old email clients like lotus notes
+    """
     content = content.strip()
     tree = html.document_fromstring(content)
+    regex = '^(https?|ftp)://'
     for node in tree.xpath('//*[@src]'):
         src = node.get('src')
-        if src.startswith('https://'):
-            url = src.replace('https://', 'http://')
-            node.set('src', url)
+        url = re.sub(regex, '//', src)
+        node.set('src', url)
     for node in tree.xpath('//*[@href]'):
         href = node.get('href')
-        if href.startswith('https://'):
-            url = href.replace('https://', 'http://')
-            node.set('href', url)
+        url = re.sub(regex, '//', href)
+        node.set('href', url)
     data = etree.tostring(tree,
                           pretty_print=True,
                           encoding="utf-8",
